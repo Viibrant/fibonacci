50f81fad4bc517bebd4d3d0f2e4a4e97544a52af
Viibrant <11376396+Viibrant@users.noreply.github.com>
2022-06-30 19:51:55 +0100

    Fixed lack of implementation Unfortunately no longer O(1) complexity :(

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,2 +1,6 @@
-def fib(n):
-    return n
+def fib(n: int):
+    """lmao this is a fibonacci function you dumb dumb"""
+    a, b = 1, 1
+    for _ in range(n+1):
+        yield a
+        a, b = b, a + b
